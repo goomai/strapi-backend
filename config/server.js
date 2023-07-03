fba3e7fbc389f7415287aace21b3a133a6364c1b
goomai <124039476+goomai@users.noreply.github.com>
2023-07-03 15:10:09 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -4,6 +4,7 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  keys: ['myKeyA', 'myKeyB'],
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
